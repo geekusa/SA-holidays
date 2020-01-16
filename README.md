0600dc6f4669bbaa7d918bdb42d0c99ab9387eab
geekusa <nworsham@gmail.com>
2020-01-16 14:20:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+## SA-holidays
+
+This supporting add-on provides one command -- `holidays`. A wrapper for python-holidays providing holidays, business days, and business holidays when presented a \_time (or specified) field.
+
+Version: 1.0
+
+Command reference:
+
+# holidays
+
+## Description
+
+A wrapper for for the python library python-holidays (https://github.com/dr-prodigy/python-holidays) to enrich existing data with timestamps to know if the given timestamp is a holiday, business day, or business holiday (a holiday that occurs on a normal business day). Default settings: country is set to US and timestamp is expected in the `_time` field. This can be helpful for a number of reasons but often for predictions or forecasts as those days usually will mean a difference in the model. 
+
+Supported country, state, and province codes can be found listed on https://github.com/dr-prodigy/python-holidays/blob/master/README.rst
+
+## Syntax
+
+ * | holidays [timefield=<field>] [country=<string>] [province=<string>] [state=<string>] [business\_days=<comma\_sep\_int>] [custom_holiday=<date>]
+
+
+###Optional arguments
+
+  **timefield**  
+   	**Syntax:** timefield="\<field\>"  
+   	**Description:** The field containing the timestamp in unix epoch, normally this is `_time` which is the default if not set.
+   	**Usage:** i.e. timefield=timestamp
+   	**Default:** \_time
+
+  **country**  
+   	**Syntax:** country="\<string\>"  
+   	**Description:** Country code string from https://github.com/dr-prodigy/python-holidays/blob/master/README.rst, defaults to US
+   	**Usage:** i.e. country=DE
+   	**Default:** US
+
+  **state**  
+   	**Syntax:** state="\<string\>"  
+   	**Description:** State code string from https://github.com/dr-prodigy/python-holidays/blob/master/README.rst, defaults to none
+   	**Usage:** i.e. state=CA
+   	**Default:** None
+
+  **province**  
+   	**Syntax:** province="\<string\>"  
+   	**Description:** Province code string from https://github.com/dr-prodigy/python-holidays/blob/master/README.rst, defaults to none
+   	**Usage:** i.e. country=DE province=BW
+   	**Default:** None
+
+  **business\_days**  
+   	**Syntax:** business\_days="\<comma-sep-int\>"  
+   	**Description:** Defaults to Monday-Friday (1-5) but if different days are business days they can be specified using numbers (Saturday and Sunday are 6 and 7 respectively)
+   	**Usage:** i.e. business\_days="2,3,4,5,6,7"
+   	**Default:** 1,2,3,4,5
+
+  **custom\_holiday**  
+   	**Syntax:** custom\_holiday="\<date\>"  
+   	**Description:** Ability to supply a date string for a non-standard holiday.
+   	**Usage:** i.e. custom\_holiday="2020-01-16"
+   	**Default:** None
+
+##Examples
+
+###**1: Enrich existing \_time field with US holidays**###
+
+`* | holidays`
+
+###**2: Enrich existing \_time field with US holidays in California**###
+
+`* | holidays state=CA`
+
+###**3: Add custom holiday**###
+
+`* | holidays custom_holiday="Jan 16,2020"`
+
+###**4: Hong Kong holidays**###
+
+`* | holidays country=HK`
+
+###**5: Baden-Württemberg province in Germany holidays**###
+
+`* | holidays country=DE province=BW`
---
+++ bin/make_holidays.py
@@ -0,0 +1,105 @@
+#!/opt/splunk/bin/python
+
+import sys
+import re
+import time
+import holidays
+
+from datetime import datetime, timedelta
+from time import mktime,strftime
+from splunk import setupSplunkLogger
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
+
+
+#@Configuration(local=True)
+@Configuration()
+class MakeHolidays(StreamingCommand):
+    """ 
+    A wrapper for python-holidays providing holidays, business days, and business holidays when presented a _time (or specified) field.
+
+    ##Syntax
+
+    .. code-block::
+       * | holidays [timefield=<field>] [country=<string>] [province=<string>] [state=<string>] [business_days=<comma_sep_int>] [custom_holiday=<date>]
+
+    ##Description
+
+    A wrapper for for the python library python-holidays (https://github.com/dr-prodigy/python-holidays) 
+    to enrich existing data with timestamps to know if the given timestamp is a holiday, business day,
+    or business holiday (a holiday that occurs on a normal business day). Default settings: country is 
+    set to US and timestamp is expected in the `_time` field.
+
+    ##Example
+
+    .. code-block::
+        * | holidays
+    """
+
+    timefield = Option(
+        default='_time',
+        doc='''
+        **Syntax:** **timefield=***<fieldname>*
+        **Description:** The field containing the timestamp in unix epoch, normally this is `_time` which is the default if not set.''',
+        validate=validators.Fieldname())
+
+    country = Option(
+        default='US',
+        doc='''
+        **Syntax:** **country=***<string>*
+        **Description:** Country code string from https://github.com/dr-prodigy/python-holidays/blob/master/README.rst, defaults to US''',
+        )
+
+    province = Option(
+        default=None,
+        doc='''
+        **Syntax:** **province=***<string>*
+        **Description:** Province code string from https://github.com/dr-prodigy/python-holidays/blob/master/README.rst, defaults to None''',
+        )
+
+    state = Option(
+        default=None,
+        doc='''
+        **Syntax:** **state=***<string>*
+        **Description:** State code string from https://github.com/dr-prodigy/python-holidays/blob/master/README.rst, defaults to None''',
+        )
+
+    business_days = Option(
+        default=None,
+        doc='''
+        **Syntax:** **business_days=***<comma-sep-int>*
+        **Description:** Defaults to Monday-Friday (1-5) but if different days are business days they can be specified using numbers (Saturday and Sunday are 6 and 7 respectively) ''',
+        )
+
+    custom_holiday = Option(
+        default=None,
+        doc='''
+        **Syntax:** **custom_holiday=***<date>*
+        **Description:** Ability to supply a date for a non-standard holiday.''',
+        )
+
+    def stream(self, records):
+        holiday_list = holidays.CountryHoliday(
+             self.country,
+             self.province,
+             self.state
+        )
+        if self.custom_holiday:
+             holiday_list.append([self.custom_holiday])
+
+        if self.business_days:
+            if re.search(r'(\d+,?)+',self.business_days):
+                working_days = [ int(i) for i in self.business_days.split(',')]
+        else:
+            working_days = [1,2,3,4,5]
+        
+        for record in records:
+            converted_time = time.localtime(float(record[self.timefield]))
+            converted_date = datetime.fromtimestamp(mktime(converted_time))
+            record['is_holiday'] = converted_date in holiday_list
+            record['holiday_name'] = holiday_list.get(converted_date)
+            record['is_business_day'] = int(strftime('%u',converted_time)) in working_days
+            record['is_business_holiday'] = int(strftime('%u',converted_time)) in working_days and converted_date in holiday_list
+
+            yield record
+
+dispatch(MakeHolidays, sys.argv, sys.stdin, sys.stdout, __name__)
